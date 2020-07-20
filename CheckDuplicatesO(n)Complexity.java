7a4c599d594278c1123ba037c882daa8b9fca734
Anshul692 <44148237+Anshul692@users.noreply.github.com>
2020-07-20 12:48:33 +0530

    Update CheckDuplicatesO(n)Complexity.java

--- CheckDuplicatesO(n)Complexity.java
+++ CheckDuplicatesO(n)Complexity.java
@@ -9,12 +9,12 @@ import java.util.*;
 public class Main
 {
 	public static void main(String[] args) {
-		Scanner Input = new Scanner(System.in);
 		int[] Array = new int[Input.nextInt()];
 		ScanArray(Array,Input);
 		CheckDuplicates(Array);
 	}
-	static void ScanArray(int[] Array,Scanner Input){
+	static void ScanArray(int[] Array){
+ 	    Scanner Input = new Scanner(System.in);
 	    for(int i = 0 ; i < Array.length ; i++)
 	       Array[i] = Input.nextInt();
 	}
