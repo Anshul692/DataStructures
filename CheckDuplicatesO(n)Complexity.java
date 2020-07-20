5f6abcc2d5291bb90973a47e598be134645a4bb0
Anshul692 <44148237+Anshul692@users.noreply.github.com>
2020-07-20 11:39:24 +0530

    Add files via upload

---
+++ CheckDuplicatesO(n)Complexity.java
@@ -0,0 +1,34 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner Input = new Scanner(System.in);
+		int[] Array = new int[Input.nextInt()];
+		ScanArray(Array,Input);
+		CheckDuplicates(Array);
+	}
+	static void ScanArray(int[] Array,Scanner Input){
+	    for(int i = 0 ; i < Array.length ; i++)
+	       Array[i] = Input.nextInt();
+	}
+	static void CheckDuplicates(int[] Array){
+	    HashSet Checker = new HashSet();
+	    for(int i = 0 ; i < Array.length ; i++)
+	    {
+	        if(Checker.contains(Array[i]))
+	           System.out.print(Array[i] + " ");
+	        else if(Array[i]<=0)
+	        System.out.print("zero or negative number found" + Array[i]);
+	        else
+	           Checker.add(Array[i]);
+	    }
+	}
+}
+
