00d2814c362960b2d95f65ff09859d42fc50fff7
Anshul692 <44148237+Anshul692@users.noreply.github.com>
2020-07-23 11:54:01 +0530

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -14,23 +14,17 @@ class Main
 	public void push(int new_data) 
 	{ 
 		Node new_node = new Node(new_data); 
-
 		new_node.next = head; 
-
 		head = new_node; 
 	} 
 
 	public void insertAfter(Node prev_node, int new_data) 
 	{ 
 		if (prev_node == null) 
-		{ 
 			System.out.println("Previous node cannot be null"); 
-	} 
 
 		Node new_node = new Node(new_data); 
-
 		new_node.next = prev_node.next; 
-
 		prev_node.next = new_node; 
 	} 
 	
